bc11c5a54ca82fd4571745db239b4f425c406aa1
17602007679 <1085591814@qq.com>
2025-06-03 16:28:35 +0800

    20250613

--- src/views/tender/enroll/form/DetailInfo.vue
+++ src/views/tender/enroll/form/DetailInfo.vue
@@ -4,15 +4,44 @@
       <el-row :gutter="20">
         <el-table class="list-main" height="300px" :data="list" border size="mini"
                   :highlight-current-row="true">
-          <el-table-column
-            v-for="(t,i) in columns"
-            :key="i"
-            align="center"
-            :prop="t.name"
-            :label="t.text"
-            v-if="t.default!=undefined?t.default:true"
-            :width="t.width?t.width:''"
-          ></el-table-column>
+          <template v-for="(t,i) in columns">
+            <el-table-column
+              :key="i"
+              v-if="t.default == 'escape'?true:false"
+              :label="t.text"
+              :width="t.width?t.width:''"
+              align="center"
+            >
+              <template width="90" slot-scope="scope">
+                <span>{{scope.row.status == '1'?'已缴费':'未缴费'}}</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              :key="i"
+              v-else-if="t.default == 'img'?true:false"
+              :label="t.text"
+              :width="t.width?t.width:''"
+              align="center"
+            >
+              <template width="90" slot-scope="scope">
+                <el-image
+                  v-if="scope.row[t.name]"
+                  style="width: 100px; height: 100px"
+                  :src="scope.row[t.name]"
+                  :preview-src-list="[scope.row[t.name]]">
+                </el-image>
+                <span v-else>无</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              :key="i"
+              v-else
+              align="center"
+              :prop="t.name"
+              :label="t.text"
+              :width="t.width?t.width:''"
+            ></el-table-column>
+          </template>
         </el-table>
       </el-row>
     </el-form>
@@ -38,8 +67,11 @@ export default {
         {text: '联系人', name: 'transactorsName'},
         {text: '联系电话', name: 'contactNumber'},
         {text: '邮箱', name: 'mailbox'},
-        {text: '营业执照', name: 'businessCertificate'},
-        {text: '相关资质证明', name: 'relevantInformation'}],
+        {text: '是否缴费', name: 'status', default: 'escape'},
+        {text: '报名费', name: 'regFee'},
+        {text: '报名日期', name: 'createDate'},
+        {text: '营业执照', name: 'businessCertificate', default: 'img' },
+        {text: '相关资质证明', name: 'relevantInformation', default: 'img' }],
       pidS: [],
       rules: {},
     };
